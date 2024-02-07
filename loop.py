197cf88178c7222692f94fd6767bba5172ff9c8c
Fai <faiabdulla123@gmail.com>
2024-02-07 21:15:04 +0300

    loop2

---
+++ loop.py
@@ -0,0 +1,7 @@
+while True:
+    Answer=input("What is product of 7*24?")
+    if Answer=="168":
+        print("You answer this Question correctly")
+    break
+else:
+    print("Your Answer is wrong try again")
---
+++ loopbonus.py
@@ -0,0 +1,7 @@
+n= int(input("Enter positive numper:"))
+sum_evens =0
+ 
+for num in range(1,n+1):
+     if num %2 == 0:
+         sum_evens += num
+         print("The sum of even number between 1 an","is:", sum_evens)
