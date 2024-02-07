69d48aeca42069af57622e6ff8a165d78b58a6c4
rahaf <rahaf09876509@gmail.com>
2024-02-08 01:43:55 +0300

    lab4

--- loop.py
+++ loop.py
@@ -1,14 +1,14 @@
 
-numbers=range(45,210)
-for number in numbers:
+number=range(45,210)
+for number in number:
         print(number)
         if number == 100:
             continue
-        if number == 205:
+        elif number == 205:
              break
         print(number)
-        super:str("what is the product og 7*24")
-        while input(super) !="168":
+        super=int(input("what is the product og 7*24"))
+        while super !="168":
             print("Your answer is worng try agine")
         else:
             print("You answered this Question correctly")
