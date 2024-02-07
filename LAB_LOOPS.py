c6c35c7117d2044efb987bdf974bb0394b4c6e3e
WissamZaidi <wzaidi@stu.kau.edu.sa>
2024-02-07 14:12:15 +0300

    ✅ Done

---
+++ LAB_LOOPS.py
@@ -0,0 +1,17 @@
+for i in range(45, 210):
+    match i:
+        case 100:
+            continue
+        case 205:
+            break
+        case _:
+            print(i)
+question = 0
+answer = 7*24
+while question != answer:
+    question = int(input("What is the product of 7 * 24?"))
+    if question == answer:
+        print("You answered this Question correctly")
+        break
+    else:
+        print("Your Answer is wrong try again..")
