c900cc3a700b1f7b074468eb80566fb34680ecfc
Shekah <shekhahathlol@gmail.com>
2024-02-08 01:04:15 +0300

    completed answer

--- Lab4.py
+++ Lab4.py
@@ -1,6 +1,10 @@
-# 1) Using range(),  make a range from 45 to 210, using a for loop iterate over the sequence and print the elements. Skip the number 100 and break the loop at 205
-for i in range(45,210):
-    print(i)
+# 1) Using range(),  make a range from 45 to 210, using a for loop iterate over the sequence and print the elements.Skip the number 100 and break the loop at 205
+for number in range(45,210):
+    if number == 100:
+        continue
+    if number == 205:
+        break
+    print(number)
     
 # 2) Using a while loop and input , do the following :
 # - Ask the the user : "what is the product of 7 * 24 ?"
