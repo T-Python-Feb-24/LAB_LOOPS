740d29ca87b315a15d00b105a62d49147afdd895
rahaf <rahaf09876509@gmail.com>
2024-02-07 14:58:35 +0300

    loop

---
+++ loop.py
@@ -0,0 +1,17 @@
+
+numbers=range(45,210)
+for number in numbers:
+        print(number)
+        if number == 100:
+            continue
+        if number == 205:
+             break
+        print(number)
+        super:str("what is the product og 7*24")
+        while input(super) !="168":
+            print("Your answer is worng try agine")
+        else:
+            print("You answered this Question correctly")
+
+
+            
